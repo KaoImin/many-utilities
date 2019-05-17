c8f0d949f1aa2702d39657e86c6c09cd72d10c6b
KaoImin <kaoimin@qq.com>
2019-05-17 16:12:04 +0800

    develop number theory

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//!
+//!
+
+// #![deny(missing_docs)]
+
+///
+pub mod number_theory;
+///
+pub mod segment_tree;
